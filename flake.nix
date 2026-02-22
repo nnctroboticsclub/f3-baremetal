8c35b37f5d58ba1eaa9873af11e8852817191e6e
syoch <syoch64@gmail.com>
2026-02-22 16:12:58 +0900

    Update: Added Nano

--- flake.nix
+++ flake.nix
@@ -2,9 +2,15 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
 
   inputs.roboenv.url = "git+ssh://git@github.com/nnctroboticsclub/roboenv-nix.git";
+  inputs.nano.url = "git+ssh://git@github.com/nnctroboticsclub/nano.git";
 
   outputs =
-    { nixpkgs, roboenv, ... }:
+    {
+      nixpkgs,
+      roboenv,
+      nano,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -18,9 +24,11 @@
         version = "v0.2.0";
         src = ./.;
 
-        cmakeBuildInputs = [ ];
-        nativeBuildInputs = [
-          pkgs.gtest
+        cmakeBuildInputs = [
+          rpkgs.cmsis5-device-f3
+          rpkgs.clang-arm-toolchain
+          rpkgs.roboenv-loader
+          nano.packages.${system}.default
         ];
       };
     };
