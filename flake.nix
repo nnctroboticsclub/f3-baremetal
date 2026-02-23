08e440c5ec771bf1f88cfe3f8b36ee961f073b93
syoch <syoch64@gmail.com>
2026-02-23 20:07:04 +0900

    Fix: Fixed some name

--- flake.nix
+++ flake.nix
@@ -1,21 +1,15 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
-
   inputs.roboenv.url = "git+ssh://git@github.com/nnctroboticsclub/roboenv-nix.git";
   inputs.nano.url = "git+ssh://git@github.com/nnctroboticsclub/nano.git";
 
   outputs =
     {
-      nixpkgs,
+      self,
       roboenv,
       nano,
-      ...
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
       rpkgs = roboenv.legacyPackages.${system};
     in
     {
